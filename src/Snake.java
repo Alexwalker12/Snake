8e4f68973e6aadb314e6d72dbe88fb688bcd052b
AlexWalker <alexjohn.walker@o2.com>
2017-10-05 14:45:12 +0100

    initial Commit

---
+++ src/Snake.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Snake extends JFrame {
+
+    public Snake() {
+
+        add(new Board());
+
+        setResizable(false);
+        pack();
+
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame ex = new Snake();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
